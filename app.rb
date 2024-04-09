9c2c33593c29856bf2ac50ca8de6270e37b91e09
christianeron <christianeron1@gmail.com>
2024-04-09 22:44:41 +0000

    initial

--- app.rb
+++ app.rb
@@ -1,9 +1,52 @@
 require "sinatra"
 require "sinatra/reloader"
 
+#rakegrade 3waisvokTmjsMiMGnsL2XT6K
+
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+erb(:square)
 end
+
+get("/square/new") do
+erb(:square)
+end
+
+get("/square/results") do
+  @number = params.fetch("number").to_f
+  @square = @number ** 2
+erb(:square_results)
+end
+
+get("/square_root/new") do
+  erb(:square_root)
+  end
+  
+  get("/square_root/results") do
+    @user_number = params.fetch("user_number").to_f
+    @square_root = @user_number ** 0.5.to_f
+  erb(:square_root_results)
+  end
+
+get("/payment/new") do
+  erb(:payment)
+  end
+  
+  get("/payment/results") do
+    @APR = params.fetch("user_apr").to_f
+    @num_years = params.fetch("user_years").to_i
+    @principal = params.fetch("user_pv").to_f
+    @payment = 0
+  erb(:payment_results)
+  end
+
+get("/random/new") do
+  erb(:random)
+  end
+  
+  get("/random/results") do
+    @user_min = params.fetch("user_min").to_f
+    @user_max = params.fetch("user_max").to_f
+    @random_result = rand(@user_min...@user_max).to_f
+  erb(:random_results)
+  end
+  
